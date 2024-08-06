d54f0e75481beb7c35dbd4153dc8b9dba877107d
Nelamin2 <nidalelamin@gmail.com>
2024-08-07 02:52:40 +0300

    task 1,2

--- 0x01-Basic_authentication/SimpleAPI/0x01-Basic_authentication/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/0x01-Basic_authentication/v1/views/index.py
@@ -4,6 +4,21 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - the status of the API
+    """
+    abort(403)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the status of the API
+    """
+    abort(401)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
