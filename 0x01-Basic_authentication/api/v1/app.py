ac4717e414c6c320b453d901dd65e927ca085a22
Nelamin2 <nidalelamin@gmail.com>
2024-08-07 05:11:23 +0300

    tasks

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,18 +2,18 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
+
 
 if getenv('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
@@ -25,23 +25,38 @@ elif getenv('AUTH_TYPE') == "basic_auth":
 
 
 @app.before_request
-def filter_request():
+def before_request():
     if auth is None:
         return
 
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-    if not auth.require_auth(request.path, excluded_paths):
+    # Define public paths
+    public_paths = ['/api/v1/status',
+                    '/api/v1/unauthorized', '/api/v1/forbidden']
+
+    if request.path in public_paths:
         return
 
+    # Check authorization header
     if auth.authorization_header(request) is None:
-        abort(401)
+        abort(401)  # Unauthorized
 
+    # Check current user
     if auth.current_user(request) is None:
-        abort(403)
+        abort(403)  # Forbidden
+
+
+@app.errorhandler(401)  # Unauthorized
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)  # Forbidden
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 @app.errorhandler(404)
@@ -51,28 +66,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized_request(error) -> str:
-    '''Unauthorized requests
-    '''
-
-    return jsonify({
-        "error": "Unauthorized"
-    }), 401
-
-
-@app.errorhandler(403)
-def forbidden_request(error) -> str:
-    '''Forbidden request
-    '''
-
-    return jsonify({
-        "error": "Forbidden"
-    }), 403
-
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
