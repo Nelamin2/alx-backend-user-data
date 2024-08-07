d09322e9011fc89aa39ae57af6ecd07006e8f575
Nelamin2 <nidalelamin@gmail.com>
2024-08-07 05:14:33 +0300

    tasks

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,19 @@ class BasicAuth(Auth):
     Args:
         Auth (class): Parent authentication class
     """
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """returns the Base64 part of the Authorization header
+        for a Basic Authentication
+        Args:
+            authorization_header (str): auth_header
+        Returns:
+            str: base64 part of header
+        """
+        if not (authorization_header and isinstance(authorization_header, str)
+                and authorization_header.startswith('Basic ')):
+            return None
+
+        return authorization_header[6:]
