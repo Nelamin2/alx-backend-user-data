853c6b45fc28181a951b66454a966a9c4cb61a08
Nelamin2 <nidalelamin@gmail.com>
2024-08-07 03:52:11 +0300

    tasks

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -12,7 +13,15 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+# Check environment variable to configure auth
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+if AUTH_TYPE:
+    # Dynamically import Auth class based on AUTH_TYPE
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(401)  # Unauthorized
 def unauthorized(error) -> str:
