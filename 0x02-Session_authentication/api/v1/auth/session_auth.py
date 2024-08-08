3c5d7e1b877413f23b45a11d421a3f4fa809b9a0
Nelamin2 <nidalelamin@gmail.com>
2024-08-08 10:03:14 +0300

    tasks

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,6 @@
 #!/ usr/ bin / env python3
 """  session_auth module    """
 from api.v1.auth.auth import Auth
-from models.user import User
-import base64
-from typing import TypeVar
-from flask import request
-from os import getenv
 
 class SessionAuth(Auth):
     """ SessionAuth class
