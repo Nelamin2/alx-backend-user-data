05ef71d285a52395555a08be59bf0832183fd058
Nelamin2 <nidalelamin@gmail.com>
2024-08-16 00:14:02 +0300

    tasks

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,19 +1,24 @@
-#!/usr/ bin/env python3
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+#!/usr/bin/env python3
+"""
+Create a SQLAlchemy model named `User`
+for database table named `users`
+"""
 
-# Create a base class for our models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
 Base = declarative_base()
 
+
 class User(Base):
+    """ SQLAlchemy model named User
+    for database table named users
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=True)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-# Example engine (usually you would connect to an actual database)
-engine = create_engine('sqlite:///:memory:')
-Base.metadata.create_all(engine)
