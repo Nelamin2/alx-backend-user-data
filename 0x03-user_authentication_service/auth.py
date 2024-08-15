0cc13e48148537ad3271ea5cebc2fcfc7747ec5f
Nelamin2 <nidalelamin@gmail.com>
2024-08-16 02:27:04 +0300

    tasks

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,3 +42,21 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+   def valid_login(self, email: str, password: str) -> bool:
+        """Expect email and password required arguments
+        Returns a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
+
+    def _generate_uuid() -> str:
+    """Returns string repr of a new UUID
+    Use uuid module
+    """
+    return str(uuid.uuid4())
